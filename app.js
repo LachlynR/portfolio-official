2acd4299a117a7e14def7fea38fdc7e4654005c2
LachlynR <lachyreynolds@outlook.com>
2020-05-14 15:38:25 +1000

    Added jQuery slideToggle() to nav

---
+++ app.js
@@ -0,0 +1,7 @@
+// jQuery which slides the nav up and down when the burger icon is clicked
+
+$(document).ready(function() {
+    $(".fa-bars").click(() => {
+        $("nav").slideToggle('slow');
+    })
+})
