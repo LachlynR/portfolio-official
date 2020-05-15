e966cd1c91faf51fd5f50403eae36f675215c26e
LachlynR <lachyreynolds@outlook.com>
2020-05-15 18:57:34 +1000

    Contact Mobile Complete

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // jQuery which slides the nav up and down when the burger icon is clicked
 
-$(document).ready(function() {
-    $(".fa-bars").click(() => {
-        $("nav").slideToggle('slow');
-    })
-})
-
+$(document).ready(function () {
+  $(".fa-bars").click(() => {
+    $("nav").slideToggle("slow");
+  });
+});
